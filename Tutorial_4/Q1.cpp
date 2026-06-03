3ca589a67262b988b1b77dd39faf1959d7cf5044
Syahir-Code <sy4h.0fficial@gmail.com>
2026-06-03 12:04:00 +0800

    completed T4Q2

--- Tutorial_4/Q1.cpp
+++ Tutorial_4/Q1.cpp
@@ -9,11 +9,15 @@ class Purchase {
         string itemName;
         float total = 0;
 
+        int final_qty = 0;
+        float final_price = 0.0;
+
     public:
         Purchase (string name, int quantity, float p);
         void set_data (string name, int quantity, float p);
         float calculate ();
         void print ();
+        void printtotal();
 };
 
 
@@ -31,25 +35,65 @@ void Purchase::set_data (string name, int quantity, float p) {
 
 float Purchase::calculate () {
     total = qty*price;
+    final_qty += qty;
+    final_price += total;
     return total;
 }
 
+
 void Purchase::print () {
-    cout << "Name: " << itemName << endl;
-    cout << "Quantity: " << qty << endl;
-    cout << "Price: " << price << endl;
-    cout << "Total: "  <<  total << endl;
-    cout << endl;
+    cout << "=========================" << endl;
+    cout << "Subtotal  : RM ";
+    cout << total << endl;
+    cout << "=========================" << "\n\n";
+}
+
+void Purchase::printtotal () {
+    cout << "=========================" << endl;
+    cout << "       GRAND TOTAL        " << endl;
+    cout << "=========================" << endl;
+
+    cout << "Total number of items   : ";
+    cout << final_qty << endl;
+
+    cout << "Total amount to be paid : RM ";
+    cout << final_price << endl;
 }
 
 int main () {
-    Purchase p1 ("Apple", 20, 0.50);
-    p1.calculate();
-    p1.print();
+    string N;
+    int Q;
+    float P;
 
-    p1.set_data ("Banana", 30, 0.25);
-    p1.calculate();
-    p1.print();
+    Purchase p1 (" ", 0, 0.0); 
 
+    cout << "=========================" << endl;
+    cout << "          WELCOME         " << endl;
+    cout << "=========================" << endl;
+
+    do {
+        cout << "Enter item <'Q' to quit> : ";
+        cin >> N;
+
+        if (N == "Q") {
+            p1.printtotal();
+            return 0;
+        }
+
+        else {
+            cout << "Enter quantity : ";
+            cin >> Q;
+
+            cout << "Enter price    : RM ";
+            cin >> P;
+
+            p1.set_data (N, Q, P);
+            p1.calculate();
+            p1.print();
+        }
+
+    } while (true);
+
+    
     return 0;
 }
--- Tutorial_4/Q2.cpp
+++ Tutorial_4/Q2.cpp
@@ -4,15 +4,58 @@ using namespace std;
 
 class Temperature {
     private:
+        double tempCel;
+        double tempFar;
 
     public:
-        Temperature ();
+        Temperature () {
+            tempCel = 0.0;
+            tempFar = 32.0;
+        }
 
+        Temperature (char unit, double temp) {
+            if  (unit == 'C') {
+                tempCel = temp;
+                tempFar = (9*tempCel/5) + 32;
+            }
+
+            else if (unit == 'F') {
+                tempFar = temp;
+                tempCel = (tempFar - 32)*5/9;
+            }
+            else {
+                tempCel = 0.0;
+                tempFar = 32.0;
+            }
+        }
+    
+        double getCels () {
+            return  tempCel;
+        }
+
+        double getFar () {
+            return tempFar;
+        }
+
+        void equal (Temperature t) {
+            tempCel = t.tempCel;
+            tempFar = t.tempFar;
+        }
 };
 
 int main () {
-    Temperature t1('C', 100), t2('F', 100);
+    Temperature t1('C',100), t2('F',100);
 
-    cout << "t1 = " << t1.getCels() << "C\n";
+    cout<<"t1 = "<<t1.getCels()<<" C\n";
+    cout<<"t1 = "<<t1.getFar()<<" F\n";
+
+    cout<<"t2 = "<<t2.getCels()<<" C\n";
+    cout<<"t2 = "<<t2.getFar()<<" F\n";
+
+    t1.equal(t2); // assign value in t2 to t1
+    cout<<"After assigning t2 to t1"<<endl;
+    cout<<"t1 = "<<t1.getCels()<<" C\n";
+    cout<<"t1 = "<<t1.getFar()<<" F\n";
+
     return 0;
 }
